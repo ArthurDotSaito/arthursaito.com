a4723aaab6579fc75e43147d4e27c4ddb00a974b
Arthur Saito <arthur_saito1998@hotmail.com>
2023-11-03 22:19:23 -0300

    chore: adding Chakra UI

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from 'react';
 import { PrimeReactProvider } from 'primereact/api';
+import { ChakraProvider } from '@chakra-ui/react';
 
 interface IProps {
   children: ReactNode;
 }
 
 const Providers = ({ children }: IProps) => {
-  return <PrimeReactProvider value={{ ripple: true }}>{children}</PrimeReactProvider>;
+  return (
+    <ChakraProvider>
+      <PrimeReactProvider value={{ ripple: true }}>{children}</PrimeReactProvider>;
+    </ChakraProvider>
+  );
 };
 
 export { Providers };
