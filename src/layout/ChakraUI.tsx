bd0e869171467e15c01b283628c6aecfa13bd8d1
Arthur Saito <arthur_saito1998@hotmail.com>
2023-11-03 23:19:31 -0300

    test Box Chakra

---
+++ src/layout/ChakraUI.tsx
@@ -0,0 +1,3 @@
+export { Box } from '@chakra-ui/react';
+
+export { useColorModeValue } from '@chakra-ui/react';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
+import { Box, useColorModeValue } from '@/layout/ChakraUI';
+
 export default function Index() {
-  return <>aaa</>;
+  return (
+    <>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={'whiteAlpha.200'}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I'm Arthur Saito
+      </Box>
+    </>
+  );
 }
