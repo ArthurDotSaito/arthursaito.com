4d6fe37081edb7990dd213b38efc620b0da96703
Arthur Saito <arthur_saito1998@hotmail.com>
2023-11-05 23:28:25 -0300

    fix: small-fix  fixing avatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { AnimationContainer } from '@/components/Animation-Container';
 import ThreeJsScene from '@/components/lib/ThreeScene';
 import { Avatar, Box } from '@/layout/ChakraUI';
+import { chakra } from '@chakra-ui/react';
 import TypewriterComponent from 'typewriter-effect';
+import Image from 'next/image';
 
 export default function Index() {
+  const ProfileImage = chakra(Image, {
+    shouldForwardProp: (prop: string) => ['width', 'height', 'src', 'alt'].includes(prop),
+  });
+
   return (
     <>
       <AnimationContainer>
@@ -28,7 +34,10 @@ export default function Index() {
         />
       </Box>
       <Box display={{ md: 'flex' }}>
-        <Avatar src="/static/arthur.jpeg"></Avatar>
+        <Box>
+          <ProfileImage src="/static/arthur.jpeg" alt="arthur-saito" width="300" height="300"></ProfileImage>
+        </Box>
+        <Box>Arthur é legal</Box>
       </Box>
     </>
   );
