75fbec5b0574a438526c4dd420bafd0e7e426c11
Arthur Saito <arthur_saito1998@hotmail.com>
2023-11-11 21:00:55 -0300

    finishing static content

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,3 +1,13 @@
+import { Paragraph } from '@/components/Paragraph';
+import { Box } from '@/layout/ChakraUI';
+
 export default function Notes() {
-  return 'Articles';
+  return (
+    <Box mb={14}>
+      <Paragraph>
+        I refer to this section as "notes" because the stuff here is not as well-structured as article. To be honest,
+        most of the content consists of simply notes from my Notion account.
+      </Paragraph>
+    </Box>
+  );
 }
